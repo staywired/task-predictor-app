70985d29d4b7b49bb36ea65ac1789c0a80f85db5
Nelson L Nieves <nnieves@Nelsons-MacBook-Pro.local>
2020-04-30 21:41:58 -0400

    add router to app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import './App.css';
 
 import Home from './pages';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checklist from './pages/checklist';
+
 function App() {
   return (
-    <Home />
+    <Router>
+      <Switch> 
+        <Route path='/checklist'>
+                <Checklist />
+        </Route>
+        <Route path='/'>
+                <Home />
+        </Route> 
+      </Switch>
+    </Router>     
+
   );
 }
 
---
+++ src/pages/checklist/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Checklist = () => {
+    return (
+        <h1>
+            Checklist
+        </h1>
+         
+    )
+};
+
+export default Checklist;
